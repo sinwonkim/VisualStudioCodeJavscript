ff1b97a96c41d15cbb77765c0f3e418a1e5b6f44
sinwonkim <zhfps114@gmail.com>
2019-01-12 17:24:51 +0900

    #  Array안에 object ,  Object안에 Array

--- Javascript1/index.js
+++ Javascript1/index.js
@@ -1,3 +1,20 @@
-const dayOfweek = ['Mon','Tue','wed','Thu','fri','sat','sun'];
-console.log(dayOfweek);
-console.log(dayOfweek[2]);
+//데이터를 정렬하는 방법은  Array와 Object 2가지
+
+const sinwonInfo = {
+    Name : "Sinwon",
+    age : "28",
+    gender : "Male",
+    isSmart:true,
+    Movie: ['god','evil','new'],
+    arrayInf : [
+        { Name : 'sinwon', 
+          type : 'horrible' 
+        }
+    ]
+}
+Adius = [{ Name : 'sinwon', type : 'horrible' }]
+
+
+
+console.log(sinwonInfo.arrayInf[0].Name);
+console.log(Adius[0].Name);
