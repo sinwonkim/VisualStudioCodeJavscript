8e94f3618a094111b18fbef0258d2c973c292434
sinwonkim <zhfps114@gmail.com>
2019-01-12 19:33:09 +0900

    객체 안에 함수를 만들어서 사용

--- Javascript1/index.js
+++ Javascript1/index.js
@@ -1,6 +1,17 @@
-// 함수 
-function Hello(potato){
-    console.log('Hello',potato);
+const calculator = {
+    plus : function(a,b){
+        return a + b;
+    },
+    Minus : function(a,b){
+        return a - b;
+    }
 }
 
-Hello("sinwon");
+
+//console log는 객체 
+//console.log(greetSinwon)
+ const plus = calculator.plus(5,5)
+ const minus = calculator.Minus(7,5)
+console.log(plus + minus)
+
+
