8975d33cbfdcd75bc2988fbff4c8e74c9cf5ca5b
sinwonkim <zhfps114@gmail.com>
2019-01-12 18:51:10 +0900

    # function

--- Javascript1/index.js
+++ Javascript1/index.js
@@ -1,20 +1,6 @@
-//데이터를 정렬하는 방법은  Array와 Object 2가지
-
-const sinwonInfo = {
-    Name : "Sinwon",
-    age : "28",
-    gender : "Male",
-    isSmart:true,
-    Movie: ['god','evil','new'],
-    arrayInf : [
-        { Name : 'sinwon', 
-          type : 'horrible' 
-        }
-    ]
+// 함수 
+function Hello(potato){
+    console.log('Hello',potato);
 }
-Adius = [{ Name : 'sinwon', type : 'horrible' },{Name : 'dave', type : 'humid'}];
 
-const dataArray =['a','b','c','d','e'];
-
-console.log(sinwonInfo.arrayInf[0].Name);
-console.log(Adius[1].Name);
+Hello("sinwon");
