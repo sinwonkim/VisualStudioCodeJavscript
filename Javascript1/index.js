9c1e777ffc154e903505853463546355a78ece26
sinwonkim <zhfps114@gmail.com>
2019-01-14 20:31:27 +0900

    # 헨들클릭 이벤트 발생시 컬러 변경

--- Javascript1/index.js
+++ Javascript1/index.js
@@ -1,4 +1,7 @@
 const title =  document.getElementById('title');
-title.style.color="red";
-const pTag = document.querySelector("#pTag");
-console.dir(pTag);
+
+function handleClick(){ //이벤트가 발생할 떄마다 이벤트 객체가 호출됨
+    title.style.color = "blue";
+}
+
+title.addEventListener("click",handleClick); //handleResize함수를  이벤트로 add함 
