d81f11076ebf24415400b190bbcfb7898dcc13d8
sinwonkim <zhfps114@gmail.com>
2019-01-14 19:25:33 +0900

    # document 객체

--- Javascript1/index.js
+++ Javascript1/index.js
@@ -1,17 +1,4 @@
-const calculator = {
-    plus : function(a,b){
-        return a + b;
-    },
-    Minus : function(a,b){
-        return a - b;
-    }
-}
-
-
-//console log는 객체 
-//console.log(greetSinwon)
- const plus = calculator.plus(5,5)
- const minus = calculator.Minus(7,5)
-console.log(plus + minus)
-
-
+const title =  document.getElementById('title');
+title.style.color="red";
+const pTag = document.querySelector("#pTag");
+console.dir(pTag);
